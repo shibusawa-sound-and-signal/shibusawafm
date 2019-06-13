c048da1f2357aeb24b16f642babc210093489ed9
Luke Winikates <luke.winikates@gmail.com>
2019-06-12 21:34:49 -0700

    show track titles on load

--- src/Main.elm
+++ src/Main.elm
@@ -4,8 +4,8 @@ import Browser
 import Html exposing (Html, div, p, text)
 import Html.Attributes exposing (class)
 import Http
-import List exposing (repeat)
 import Json.Decode exposing (Decoder, field, list, map2, string)
+import List exposing (repeat)
 
 
 main =
@@ -16,21 +16,27 @@ main =
         , view = view
         }
 
+
 type alias Track =
-       {
-            id: String,
-            title: String
-       }
+    { id : String
+    , title : String
+    }
 
 
-trackListDecoder : Decoder (List Track)
+type alias TrackList =
+    List Track
+
+
+trackListDecoder : Decoder TrackList
 trackListDecoder =
-  list <| map2 Track (field "id" string) (field "title" string)
+    list <| map2 Track (field "id" string) (field "title" string)
 
-getTrackList = Http.get
-              { url = "/annotated-track-list/627twzacY3mbvUUySz0qPD"
-              , expect = Http.expectJson Loaded trackListDecoder
-              }
+
+getTrackList =
+    Http.get
+        { url = "/annotated-track-list/627twzacY3mbvUUySz0qPD"
+        , expect = Http.expectJson Loaded trackListDecoder
+        }
 
 
 init : () -> ( Model, Cmd Msg )
@@ -40,29 +46,53 @@ init _ =
 
 type Model
     = Init
+    | Ready TrackList
 
 
 type Msg
-    = Loaded (Result Http.Error (List Track))
+    = Loaded (Result Http.Error TrackList)
 
 
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Loaded (Ok tracks) ->
+            ( Ready tracks, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
 
 
-placeholderCard =
+card cardTitle =
     div [ class "col-md-4" ]
         [ div [ class "card", class "mb-4", class "shadow-sm" ]
             [ div [ class "card-body" ]
-                [ p [ class "card-text" ] [ text "~~~~~~~~~~" ]
+                [ p [ class "card-text" ] [ text cardTitle ]
                 , div [ class "d-flex", class "justify-content-between", class "align-items-center" ] []
                 ]
             ]
         ]
 
 
+placeholderCard =
+    card "~~~~~~~~~~"
+
+
+trackCard track =
+    card track.title
+
+
+cards : Model -> List (Html Msg)
+cards model =
+    case model of
+        Init ->
+            repeat 12 placeholderCard
+
+        Ready tracks ->
+            List.map trackCard tracks
+
+
 view model =
     div [ class "album", class "py-5", class "bg-light" ]
         [ div [ class "container" ]
-            [ div [ class "row" ] <| repeat 8 placeholderCard ]
+            [ div [ class "row" ] <| cards model ]
         ]
