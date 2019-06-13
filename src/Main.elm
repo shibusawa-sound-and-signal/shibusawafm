7ca4c488a1ec7333607284e6dd1ad28c9bd9cfab
Luke Winikates <luke.winikates@gmail.com>
2019-06-12 21:00:08 -0700

    show placeholder cards before the data has loaded

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,50 @@
+module Main exposing (Model(..), Msg(..), init, main, placeholderCard, update, view)
+
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, div, p, text)
+import Html.Attributes exposing (class)
+import List exposing (repeat)
+
 
 main =
-  Browser.element
-  { init = init
-  , update = update
-  , subscriptions = \_ -> Sub.none
-  , view = view
-  }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
 
-init : () -> (Model, Cmd msg)
-init _ = (Init, Cmd.none)
 
-type Model = Init
+init : () -> ( Model, Cmd msg )
+init _ =
+    ( Init, Cmd.none )
 
-type Msg = Msg
+
+type Model
+    = Init
+
+
+type Msg
+    = Msg
+
 
 update msg model =
-  (model, Cmd.none)
+    ( model, Cmd.none )
+
+
+placeholderCard =
+    div [ class "col-md-4" ]
+        [ div [ class "card", class "mb-4", class "shadow-sm" ]
+            [ div [ class "card-body" ]
+                [ p [ class "card-text" ] [ text "~~~~~~~~~~" ]
+                , div [ class "d-flex", class "justify-content-between", class "align-items-center" ] []
+                ]
+            ]
+        ]
+
 
 view model =
-  div []
-    [ text "Hello Elm"
-    ]
+    div [ class "album", class "py-5", class "bg-light" ]
+        [ div [ class "container" ]
+            [ div [ class "row" ] <| repeat 8 placeholderCard ]
+        ]
